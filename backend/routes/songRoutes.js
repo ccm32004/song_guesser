684f2961804d2d97a7c1b12a8b2ee549e636e5c6
ccm32004 <chingching.ma32004@gmail.com>
2025-03-29 20:10:55 -0400

    more changes

--- backend/routes/songRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTrackSnippet } = require('../controllers/songController');
-const { getValidAccessToken } = require('../middleware/spotifyToken');
-
-router.get('/getTrackSnippet', getValidAccessToken, getTrackSnippet);
-
-module.exports = router;
