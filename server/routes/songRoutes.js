75e01a186817bade3701161bd63fe9cf833e5aa7
ccm32004 <chingching.ma32004@gmail.com>
2025-03-30 20:22:52 -0400

    rn

--- server/routes/songRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getTrackSnippet } from '../controllers/songController.js';
-import { getValidAccessToken } from '../middleware/spotifyToken.js';
-
-const router = express.Router();
-
-router.get('/getTrackSnippet', getValidAccessToken, getTrackSnippet);
-
-export default router;
